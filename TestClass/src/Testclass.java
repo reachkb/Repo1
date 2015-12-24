faa5aed9fb5fac90c54c0858b01bbd2fc021325e
reachkb <Karthikbalaji.R@LTPCHE032040317.corp.mphasis.com>
2015-12-24 14:37:29 +0530

    Test commit

--- TestClass/src/Testclass.java
+++ TestClass/src/Testclass.java
@@ -3,7 +3,7 @@ public class Testclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("this is a test project");
+System.out.println("this is a test project..");
 	}
 
 }
